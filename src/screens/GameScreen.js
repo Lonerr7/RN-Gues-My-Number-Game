42b4cbc9671c35cc3fd78aa13fee0e4ccaa88e78
Lonerr7 <krabik2342@gmail.com>
2022-05-12 14:29:25 +0300

    6. 65.Adding Game Controls Buttons to the App

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,7 +1,8 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Alert } from 'react-native';
 import Title from '../components/ui/Title';
 import { useState } from 'react';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
 
 const generateRandomBetween = (min, max, exclude) => {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -13,9 +14,41 @@ const generateRandomBetween = (min, max, exclude) => {
   }
 };
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 const GameScreen = ({ userNumber }) => {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const nextGameHandler = (direction) => {
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'higher' && currentGuess > userNumber)
+    ) {
+      Alert.alert(`Don't lie!`, 'You know that it is wrong!', [
+        { text: 'Sorry', style: 'cancel' },
+      ]);
+      alert('Dont Lie!')
+      return;
+    }
+
+    if (direction === 'lower') {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  };
 
   return (
     <View style={styles.screen}>
@@ -23,7 +56,14 @@ const GameScreen = ({ userNumber }) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
-        {/* + - */}
+        <View>
+          <PrimaryButton onPress={() => nextGameHandler('lower')}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGameHandler('higher')}>
+            +
+          </PrimaryButton>
+        </View>
         <View>
           <Text>LOG ROUNDS</Text>
         </View>
